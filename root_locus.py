66a85175657ecc4fd5596b9a7f7b0609200c99fa
Zeno Zeng <zenoofzeng@gmail.com>
2014-10-29 14:41:51 +0800

    add root_locus

---
+++ root_locus.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as pyplot
+import control
+import sympy
+import numpy
+
+S = sympy.symbols('S')
+denominator = (S**2) * (S + 2)
+denominator = denominator.expand()
+denominator = sympy.poly(denominator, S).all_coeffs()
+denominator = list(map(float, denominator))
+
+numerator = [1.]
+sys = control.matlab.tf(numerator, denominator)
+r, k = control.matlab.rlocus(sys)
+pyplot.plot(k, r)
+pyplot.grid()
+pyplot.show()
